0f547ebf3a3c51adce4fee66babaf78ff1be2654
Karthik <karthik.bhata7@gmail.com>
2018-07-12 07:31:55 +0530

    print_char added as a low level printing function

--- drivers/vga.c
+++ drivers/vga.c
@@ -1,10 +1,20 @@
 #include "vga.h"
 #include "ports.h"
 
-int get_cursor_pos();
-void set_cursor_offset(int);
 
-int get_cursor_pos() {
+#define get_offset(row, col) (row*MAX_COLUMNS + col) * 2;
+
+#define get_offset_row(offset) ((offset/2)/MAX_COLUMNS)
+
+#define get_offset_column(offset) ((offset/2)-(get_offset_row(offset)*MAX_COLUMNS))
+
+
+
+int get_cursor_offset();
+void set_cursor_offset(int);
+int print_char(char, int, int, int);
+
+int get_cursor_offset() {
 	int position = 0;
 	
 	/* Ask for upper byte */
@@ -20,18 +30,9 @@ int get_cursor_pos() {
 
 
 void kprint(char *message) {
-	int iter=0, offset=0;
-	unsigned char * vide_mem = (unsigned char *) VIDEO_ADDRESS;
-	int pos = get_cursor_pos();
-	// pos *= 2;
-	for (iter=0, offset=pos; message[iter]; iter++, offset+=2) {
-		vide_mem[offset] = message[iter];
-		vide_mem[offset+1] = BLACK_ON_WHITE;
-	}
 
-	set_cursor_offset(offset);
+	return kprint_at(message, -1, -1);
 
-	return;
 }
 
 void set_cursor_offset(int offset) {
@@ -49,45 +50,48 @@ void set_cursor_offset(int offset) {
 
 void kprint_at(char *message, int row, int column) {
 
-	if (row==-1 && column==-1) {
-		kprint(message);
-		return;
+	int iter=0, offset=0;
+	unsigned char * vide_mem = (unsigned char *) VIDEO_ADDRESS;
+
+	for (iter=0; message[iter]; iter++) {
+		offset = print_char(message[iter], row, column, 0);
+		row = get_offset_row(offset);
+		column = get_offset_column(offset);
 	}
-
-	int offset = row * MAX_ROWS + column;
-
-	set_cursor_offset(offset * 2);
-
-	kprint(message);
 
 	return;
 }
 
-/*
-int print_char(char c, int r, int col, int attr) {
+
+int print_char(char c, int row, int col, int attr) {
 	unsigned char *video_mem = (unsigned char *) VIDEO_ADDRESS;
 
 	if(!attr) attr = BLACK_ON_WHITE;
+	int offset;
 
-	int offset = get_offset(row, col);
+	if(row<0 && col<0) {
+		offset = get_cursor_offset();
+		row = get_offset_row(offset);
+		col = get_offset_column(offset);
+	}
 
-	video_mem[offset] = c;
-	video_mem[offset+1] = attr;
+	if (col>=MAX_COLUMNS && row>=MAX_ROWS) {
+		offset = get_offset(MAX_ROWS-1, MAX_COLUMNS-1);
+		video_mem[offset] = 'E';
+		video_mem[offset+1] = RED_ON_WHITE;
+		return get_offset(row, col);
+	}
+
+	if (c=='\n') {
+		offset = get_offset(row, MAX_COLUMNS-1);
+	}
+	else {
+		offset = get_offset(row, col);
+		video_mem[offset] = c;
+		video_mem[offset+1] = attr;
+	}
 	offset += 2;
 
+	set_cursor_offset(offset);
 	return offset;
 }
-
-
-int get_offset(int row, int col) {
-	return (row*MAX_ROWS + col) * 2;
-}
-
-int get_offset_row(int offset) {
-	return ((offset/2)/MAX_COLUMNS);
-}
-
-int get_offset_column(int offset) {
-	return ((offset/2)-(get_offset_row(offset)*MAX_COLUMNS));
-}
-*/
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,9 +1,9 @@
 #include "../drivers/vga.h"
 
 void main() {
-	kprint("an0ne was here!");
+	kprint("an0ne was here!\n");
 	kprint("Time to rock!");
 	kprint_at("Wavin' flag!", 0, 0);
-	// kprint("an0ne was here!");
+	kprint_at("an0ne is here!", 12, 23);
 	return;
 }
